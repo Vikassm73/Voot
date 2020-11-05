880c0b2168099361cc130cc1c358028a810ff21f
Vikassm73 <41675437+Vikassm73@users.noreply.github.com>
2020-11-05 23:03:08 +0530

    Update with login
    Voot login feature added

--- default.py
+++ default.py
@@ -1,6 +1,19 @@
 """
     Voot Kodi Addon
+    Copyright (C) 2018 gujal
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
 from urlparse import parse_qsl
@@ -39,8 +52,12 @@ def router(paramstring):
             main.list_season(params['show'],params['offSet'],params['totals'])
         elif params['action'] == 'list_show':
             main.list_episodes(params['show'],params['offSet'],params['icon'])
+        elif params['action'] == 'list_extra':
+            main.list_extra(params['offSet'])
         elif params['action'] == 'play':
             main.play_video(params['video'], strqual)
+        elif params['action'] == 'Live':
+            main.list_live(params['offSet'])
     else:
         main.list_top()
 
--- resources/lib/main.py
+++ resources/lib/main.py
@@ -5,6 +5,7 @@
 
 import sys
 from urlparse import parse_qsl
+from urllib import urlencode
 import xbmc
 import xbmcgui
 import xbmcplugin
@@ -12,7 +13,9 @@ import xbmcaddon
 import re
 import requests
 import math
+import uuid
 import web_pdb
+from json import dumps
 try:
     import StorageServer
 except:
@@ -48,8 +51,39 @@ if _settings('version') != _version:
     clear_cache()
     _addon.openSettings()
 
+def refresh_login():
+    url = "https://us-central1-vootdev.cloudfunctions.net/usersV3/v3/login"
+    #web_pdb.set_trace()
+    email=_addon.getSetting('email')
+    password = _addon.getSetting('password')
+    body = {"type":"traditional","deviceId":"Windows NT 10.0","deviceBrand":"PC/MAC", "data":{"email":email,"password":password}}
+    
+    body = dumps(body)
+
+    jd = requests.post(url,headers=headers,data=body).json()
+    if jd.get('data'):
+        headers.update({'refreshToken': jd['data']['authToken']['refreshToken']})
+        headers.update({'accessToken': jd['data']['authToken']['accessToken']})
+        headers.update({'kToken': jd['data']['kToken']})
+        headers.update({'kTokenId': jd['data']['kTokenId']})
+        headers.update({'kUserId': jd['data']['kUserId']})
+        headers.update({'uId': jd['data']['uId']})
+        headers.update({'householdId': str(jd['data']['householdId'])})
+        headers.update({'ks': jd['data']['ks']})
+    return
+
 apiUrl = 'https://psapi.voot.com/media/voot/v1/'
-headers = {'User-Agent': 'okhttp/3.4.1'}
+headers = { #"User-Agent": "okhttp/3.4.1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+            "accept": "application/json, text/plain, */*",
+            "accept-encoding": "gzip, deflate, br",
+            "content-type": "application/json;charset=UTF-8",
+            "referer": "https://www.voot.com/",
+            "origin":"https://www.voot.com"
+          }
+
+refresh_login()
+
 if _settings('EnableIP') == 'true':
     headers['X-Forwarded-For'] = _settings('ipaddress')
 
@@ -70,14 +104,15 @@ def get_top():
     Get the list of countries.
     :return: list
     """
-    MAINLIST = ['Channels', 'Movies', 'Clear Cache']
+
+
+    MAINLIST = ['Channels', 'Movies','Live', 'Clear Cache']
     return MAINLIST
 
 
 def get_langs():
     """
     Get the list of languages.
-    https://apiv2.voot.com/wsv_2_3/media/assetDetails.json?tabId=movieDetail&subTabId=allMovies&rowId=733&language=Hindi%2CEnglish&sortId=mostPopular&type=more&limit=15&offSet=0
     :return: list
     """
     languages = ['Hindi','Marathi','Telugu','Kannada','Bengali','Gujarati','Tulu']
@@ -92,7 +127,29 @@ def get_langs():
     
     return langs
 
-    
+
+
+def getlicense(id):
+
+    hdr = { "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+            "content-type": "application/json",
+            "authority": "rest-as.ott.kaltura.com",
+            "accept-encoding": "gzip, deflate, br",
+            "referer": "https://www.voot.com/",
+            "origin":"https://www.voot.com"
+          }
+    url = "https://rest-as.ott.kaltura.com/v4_4/api_v3/service/multirequest"
+    #web_pdb.set_trace()
+    token=""
+    if headers.get('ks'):
+        body = {"1":{"service":"asset","action":"get","id":id,"assetReferenceType":"media","ks":headers['ks']},"2":{"service":"asset","action":"getPlaybackContext","assetId":id,"assetType":"media","contextDataParams":{"objectType":"KalturaPlaybackContextOptions","context":"PLAYBACK"},"ks":headers['ks']},"apiVersion":"5.2.6","ks":headers['ks'],"partnerId":225}
+        body = dumps(body)
+        jd = requests.post(url,headers=hdr,data=body).json()
+        for lictoken in jd['result'][1]['sources']:
+            if lictoken.get('type') == 'DASH_LINEAR_APP':
+                token=lictoken['drm'][1]['licenseURL']
+    return token
+
 def get_channels(offSet):
     """
     Get the list of channels.
@@ -100,8 +157,9 @@ def get_channels(offSet):
     """
     channels = []
     finalpg = True
+
     url = '%svoot-web/content/specific/editorial?query=include:0657ce8613bb205d46dd8f3c5e7df829&responseType=common&page=%s'%(apiUrl,offSet)
-    #web_pdb.set_trace()
+
     jd = requests.get(url,headers=headers).json()
     items = jd['result']
     for item in items:
@@ -224,7 +282,7 @@ def get_episodes(show,offSet):
     """
     episodes = []
     url = '%svoot-web/content/generic/series-wise-episode?sort=episode:desc&id=%s&&page=%s&responseType=common'%(apiUrl,show,offSet)
-    #web_pdb.set_trace()
+
     jd = requests.get(url,headers=headers).json()
     totals = jd['totalAsset']
     finalpg = True 
@@ -273,6 +331,57 @@ def get_episodes(show,offSet):
 
     return episodes
 
+def get_extra(offSet):
+    """
+    Get the list of episodes.
+    :return: list
+    """
+    episodes = []
+    url = '%svoot-web/content/specific/editorial-clone?query=include:6e5a6d3746fbfb092fd4b090f8505cd8&&page=%s&responseType=common'%(apiUrl,offSet)
+
+    jd = requests.get(url,headers=headers).json()
+    totals = jd['totalAsset']
+    finalpg = True 
+    items = jd['result']
+    for item in items:
+        title = item['seo'].get('title')
+        eid = item.get('id')
+        icon={  'poster': item['seo'].get('ogImage'),
+                'thumb': item['seo'].get('ogImage'), 
+                'icon': item['seo'].get('ogImage'),
+                'fanart': item['seo'].get('ogImage')
+                    }
+        labels = {'title': title,
+                  'genre': item.get('genres'),
+                  'cast':item.get('contributors'),
+                  'plot': item.get('fullSynopsis'),
+                  'duration': item['duration'],
+                  'tvshowtitle': item['shortTitle'],
+                  'mediatype': 'episode',
+                  'episode': item.get('episode'),
+                  'season': item.get('season'),
+                  'aired':item.get('telecastDate')[:4] + '-' + item.get('telecastDate')[4:6] + '-' + item.get('telecastDate')[6:],
+                  'year': item.get('releaseYear')
+                 }
+
+        episodes.append((title,icon,eid,labels,totals))
+
+    offSet = int(offSet)
+    totals = int(totals)
+    itemsLeft = totals - offSet * 10
+
+    if itemsLeft > 0:
+        finalpg = False
+        pages = int(math.ceil(totals/10.0))
+
+    if not finalpg:
+        title = 'Next Page.. (Currently in Page %s of %s)' % (offSet,pages)
+        offSet += 1
+
+        labels = {}
+        episodes.append((title,_icon,offSet,labels,totals))
+
+    return episodes
 def get_movies(lang,offSet,totals):
     """
     Get the list of movies.
@@ -287,7 +396,6 @@ def get_movies(lang,offSet,totals):
         finalpg = False
         pages = int(math.ceil(totals/10.0))
 
-    #url = '%smedia/assetDetails.json?tabId=movieDetail&subTabId=allMovies&rowId=733&language=%s&%s&type=more&limit=50&offSet=%s'%(apiUrl,lang,msort,offSet*50)
     url = '%svoot-web/content/generic/movies-by-language?language=include:%s&page=%s&sort=mostpopular:desc&responseType=common'%(apiUrl,lang,offSet)
     jd = requests.get(url,headers=headers).json()        
     items = jd['result']
@@ -316,9 +424,46 @@ def get_movies(lang,offSet,totals):
 
     return movies
 
+def get_live(offSet):
+    """
+    Get the list of movies.
+    :return: list
+    """
+    live = []
 
+    totals = 40
+    offSet = int(offSet)
+    finalpg = True
+    itemsLeft = totals - (offSet) * 10
+    if itemsLeft > 0:
+        finalpg = False
+        pages = int(math.ceil(totals/10.0))
 
+    url = '%svoot-web/content/specific/editorial?query=include:0657ce8613bb205d46dd8f3c5e7df829&responseType=common&page=%s'%(apiUrl,offSet)
+    jd = requests.get(url,headers=headers).json()        
+    items = jd['result']
+    for item in items:
+        title = item.get('name')
+        url = '%svoot-web/view/channel/%s?page=1&premiumTrays=false'%(apiUrl,item.get('id'))
+        jd = requests.get(url,headers=headers).json()
+        newurl=jd['trays'][0]['apiUrl']
+        url = '%svoot-web/%s&responseType=common'%(apiUrl,newurl)
+        jd = requests.get(url,headers=headers).json()
+        mid = jd['result'][0]['id'] if jd['result'] else item.get('id')
+        icon = item['seo'].get('ogImage')
+        labels = {'title': title,
+                  'genre': item.get('genres'),
+                  'mediatype': 'movie'
+                 }
+        live.append((title,icon,mid,labels))
 
+    if not finalpg:
+        title = 'Next Page.. (Currently in Page %s of %s)' % (offSet,pages)
+        offSet += 1
+        labels = {}
+        live.append((title,_icon,offSet,labels))
+
+    return live
 
 def list_top():
     """
@@ -331,7 +476,7 @@ def list_top():
         list_item = xbmcgui.ListItem(label=item)
         list_item.setInfo('video', {'title': item, 'genre': item})
         list_item.setArt({'icon': _icon,
-                          'poster': _fanart,
+                          'poster': _icon,
                           'thumb': _icon,
                           'fanart': _fanart})
         url = '{0}?action={1}&offSet=1'.format(_url, item)
@@ -371,7 +516,6 @@ def list_shows(offSet,channel,totals):
     """
     Create the list of channels in the Kodi interface.
     """
-    #web_pdb.set_trace()
     shows = cache.cacheFunction(get_shows,offSet,channel,totals)
     listing = []
     for title,icon,sid,item_id,tcount,labels in shows:
@@ -401,7 +545,6 @@ def list_season(show,offSet,totals):
     """
     Create the list of episodes in the Kodi interface.
     """
-    #web_pdb.set_trace()
     season = cache.cacheFunction(get_season,show,offSet,totals)
     listing = []
     for title,icon,sid,item_id,labels,tcount in season:
@@ -426,9 +569,15 @@ def list_episodes(show,offSet,sicon):
     """
     Create the list of episodes in the Kodi interface.
     """
-    #web_pdb.set_trace()
     episodes = cache.cacheFunction(get_episodes,show,offSet)
     listing = []
+    
+    if show == '978245' and offSet == '1':
+        list_item = xbmcgui.ListItem(label="Bigboss Extra")
+        url = '{0}?action=list_extra&offSet=1'.format(_url)
+        is_folder = True 
+        listing.append((url, list_item, is_folder))
+    
     for title,icon,eid,labels,totals in episodes:
         list_item = xbmcgui.ListItem(label=title)
         list_item.setArt(icon)
@@ -439,6 +588,29 @@ def list_episodes(show,offSet,sicon):
             url = '{0}?action=play&video={1}'.format(_url, eid)
         else:
             url = '{0}?action=list_show&show={1}&offSet={2}&totals={3}&icon={4}'.format(_url,show,eid,totals,sicon)
+            is_folder = True        
+        listing.append((url, list_item, is_folder))
+    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
+    xbmcplugin.setContent(_handle, 'episodes')
+    xbmcplugin.endOfDirectory(_handle)
+
+def list_extra(offSet):
+    """
+    Create the list of episodes in the Kodi interface.
+    """
+    episodes = cache.cacheFunction(get_extra,offSet)
+    listing = []
+
+    for title,icon,eid,labels,totals in episodes:
+        list_item = xbmcgui.ListItem(label=title)
+        list_item.setArt(icon)
+        list_item.setInfo('video', labels)
+        is_folder = False
+        if 'Next Page' not in title:
+            list_item.setProperty('IsPlayable', 'true')
+            url = '{0}?action=play&video={1}'.format(_url, eid)
+        else:
+            url = '{0}?action=list_extra&&offSet={1}'.format(_url,eid)
             is_folder = True        
         listing.append((url, list_item, is_folder))
     xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
@@ -487,8 +659,48 @@ def list_movies(lang,offSet,totals):
         listing.append((url, list_item, is_folder))
     xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
     xbmcplugin.setContent(_handle, 'movies')
-    xbmcplugin.endOfDirectory(_handle)
+    xbmcplugin.endOfDirectory(_handle, succeeded=True, updateListing=True, cacheToDisc=True)
 
+
+def list_live(offSet):
+    """
+    Create the list of episodes in the Kodi interface.
+    """
+    #channels = cache.cacheFunction(get_live,offSet)
+    listing=[]
+
+    channels = [(u'Colors Hindi', u'http://v3img.voot.com/v3Storage/channel-logos/COLORS_HINDI.png', u'805985', {'genre': [u'Drama'], 'mediatype': 'movie', 'title': u'Colors Hindi'}),
+                (u'BB Live', u'https://v3img.voot.com/resizeMedium,w_362,h_204/v3Storage/assets/BB-24hr%20(6)-1602167714949.jpg', u'998775', {'genre': [u'Drama'], 'mediatype': 'movie', 'title': u'BB Live'}),
+                (u'Rishtey Cineplex', u'http://v3img.voot.com/v3Storage/channel-logos/CPR-500x500.png', u'979936', {'genre': [], 'mediatype': 'movie', 'title': u'Rishtey Cineplex'}), 
+                (u'MTV', u'http://v3img.voot.com/v3Storage/assets/500x500_1.png', u'805989', {'genre': [u'Reality'], 'mediatype': 'movie', 'title': u'MTV'}),
+                (u'MTV Beats', u'http://v3img.voot.com/v3Storage/channel-logos/MTV_BEATS.png', u'805294', {'genre': [u'Music'], 'mediatype': 'movie', 'title': u'MTV Beats'}),
+                (u'Colors Marathi', u'http://v3img.voot.com/v3Storage/channel-logos/COLORS_MARATHI.png', u'793930', {'genre': [u'Drama'], 'mediatype': 'movie', 'title': u'Colors Marathi'}),
+                (u'Shemaroo TV', u'http://v3img.voot.com/v3Storage/channel-logos/Shemaroo-TV-logo-500x500.png', u'981709', {'genre': [], 'mediatype': 'movie', 'title': u'Shemaroo TV'}),
+                (u'Shemaroo Marathi Bana', u'http://v3img.voot.com/v3Storage/channel-logos/MarathiBana_With-tagline-500x500.png', u'981710', {'genre': [], 'mediatype': 'movie', 'title': u'Shemaroo Marathi Bana'}),
+                (u'Sonic', u'https://v3img.voot.com/resizeMedium,w_100,h_100/v3Storage/assets/Sonic-1580278480683.jpg', u'708552', {'genre': [u'Education'], 'mediatype': 'movie', 'title': u'Sonic'}),
+                (u'Nick Jr', u'https://v3img.voot.com/resizeMedium,w_100,h_100/v3Storage/assets/Nick-Jr-1580292600144.jpg', u'553909', {'genre': [u'Education'], 'mediatype': 'movie', 'title': u'Nick Jr'}),  
+                (u'Nick HD+', u'https://v3img.voot.com/resizeMedium,w_100,h_100/v3Storage/assets/Nick-1580278662856.jpg', u'639070', {'genre': [u'Education'], 'mediatype': 'movie', 'title': u'Nick HD+'}),    
+                (u'DD National', u'http://v3img.voot.com/v3Storage/channel-logos/DD-national-test.png', u'966639', {'genre': [u'Education'], 'mediatype': 'movie', 'title': u'DD National'}), 
+               ]
+
+    for title,icon,mid,labels in channels:
+        list_item = xbmcgui.ListItem(label=title)
+        list_item.setArt({'poster': icon,
+                          'icon': icon,
+                          'thumb': icon,
+                          'fanart': icon})
+        list_item.setInfo('video', labels)
+        is_folder = False
+        if 'Next Page' not in title:
+            list_item.setProperty('IsPlayable', 'true')
+            url = '{0}?action=play&video={1}'.format(_url, mid)
+        else:
+            url = '{0}?action=Live&offSet={1}'.format(_url, mid)
+            is_folder = True
+        listing.append((url, list_item, is_folder))
+    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
+    xbmcplugin.setContent(_handle, 'movies')
+    xbmcplugin.endOfDirectory(_handle)
     
 
 
@@ -497,33 +709,56 @@ def play_video(path, strqual):
     """
     Play a video by the provided path.
 
-    :param path: str
     """
-    # Create a playable item with a path to play.
-    #https://rest-as.ott.kaltura.com/v4_4/api_v3/service/multirequest
-    #{"1":{"service":"asset","action":"get","id":"972785","assetReferenceType":"media","ks":"djJ8MjI1fNc_yxGYTCYwlcBtAtOVjHbtgojBvmabh542oVQavfXhy06QldcrwoASlSLsY-qg7fT_aZJv1YvtzOKVbpuClzPJsHBjzE1IpQ60ClZpPe2Y2Yujq64I5EeKE1pFXIHTevy5YQDZogyZ4QqMH_y2VbNRX_HoqGTHtzNQvnUBuqappmMwld6FJsqXHzddlluEL9ze65fMabQR1De2sudXO13ULZMxcz83AEKWNCUDgsOD8a6key90OZln53h9w3YTu4nxprIRZKcp5ckdWoVsIgRckRafbQ59ccIz1lT7qiW5B4_Tpaz28mGDLQAoInLxbZ7nd5kCNR8F53yrqSuxhnbn0PLtpw7NKHJQsi9V-x0xJiXKb_aLgslW6KI1BpB18rNJ-m8Mh_8YA3pZiTG_-N8ugb9i8Iz0af_wJIW1dXiMlf28T_BqNJTJ5pBf2pbfcl6qI1Cwg9Tyg-zDZ2nwCzk="},"2":{"service":"asset","action":"getPlaybackContext","assetId":"972785","assetType":"media","contextDataParams":{"objectType":"KalturaPlaybackContextOptions","context":"PLAYBACK"},"ks":"djJ8MjI1fNc_yxGYTCYwlcBtAtOVjHbtgojBvmabh542oVQavfXhy06QldcrwoASlSLsY-qg7fT_aZJv1YvtzOKVbpuClzPJsHBjzE1IpQ60ClZpPe2Y2Yujq64I5EeKE1pFXIHTevy5YQDZogyZ4QqMH_y2VbNRX_HoqGTHtzNQvnUBuqappmMwld6FJsqXHzddlluEL9ze65fMabQR1De2sudXO13ULZMxcz83AEKWNCUDgsOD8a6key90OZln53h9w3YTu4nxprIRZKcp5ckdWoVsIgRckRafbQ59ccIz1lT7qiW5B4_Tpaz28mGDLQAoInLxbZ7nd5kCNR8F53yrqSuxhnbn0PLtpw7NKHJQsi9V-x0xJiXKb_aLgslW6KI1BpB18rNJ-m8Mh_8YA3pZiTG_-N8ugb9i8Iz0af_wJIW1dXiMlf28T_BqNJTJ5pBf2pbfcl6qI1Cwg9Tyg-zDZ2nwCzk="},"apiVersion":"5.2.6","ks":"djJ8MjI1fNc_yxGYTCYwlcBtAtOVjHbtgojBvmabh542oVQavfXhy06QldcrwoASlSLsY-qg7fT_aZJv1YvtzOKVbpuClzPJsHBjzE1IpQ60ClZpPe2Y2Yujq64I5EeKE1pFXIHTevy5YQDZogyZ4QqMH_y2VbNRX_HoqGTHtzNQvnUBuqappmMwld6FJsqXHzddlluEL9ze65fMabQR1De2sudXO13ULZMxcz83AEKWNCUDgsOD8a6key90OZln53h9w3YTu4nxprIRZKcp5ckdWoVsIgRckRafbQ59ccIz1lT7qiW5B4_Tpaz28mGDLQAoInLxbZ7nd5kCNR8F53yrqSuxhnbn0PLtpw7NKHJQsi9V-x0xJiXKb_aLgslW6KI1BpB18rNJ-m8Mh_8YA3pZiTG_-N8ugb9i8Iz0af_wJIW1dXiMlf28T_BqNJTJ5pBf2pbfcl6qI1Cwg9Tyg-zDZ2nwCzk=","partnerId":225}
-    
+
+    license=getlicense(path)
+
     play_item = xbmcgui.ListItem(path=path)
     vid_link = play_item.getfilename()
+
     url = 'https://apiv2.voot.com/wsv_2_3/playBack.json?mediaId=%s'%(vid_link)
     jd = requests.get(url, headers=headers).json()        
     files = jd['assets'][0]['assets'][0]['items'][0]['files']
     if strqual not in str(files):
         if 'TV Main'  in str(files):
             strqual = 'TV Main'
+        elif 'HLS_Linear_P' in str(files):
+            strqual = 'HLS_Linear_P'
         else:
             strqual = 'HLS_PremiumHD'
 
-    
-    for file in files:
-        if file.get('Format') == strqual:
-            stream_url = file.get('URL') + '|User-Agent=playkit/android-3.4.5 com.tv.v18.viola/2.1.42 (Linux;Android 5.1.1) ExoPlayerLib/2.7.0'
-            break
+    if license!="":
+        for file in files:
+            if file.get('Format') == 'DASH_LINEAR_APP':
+                stream_url = file.get('URL')
+                break
+    else:
+        for file in files:
+            if file.get('Format') == strqual:
+                stream_url = file.get('URL') + '|User-Agent=playkit/android-3.4.5 com.tv.v18.viola/2.1.42 (Linux;Android 5.1.1) ExoPlayerLib/2.7.0'
+                break
+
     if _settings('EnableIP') == 'true':
         stream_url += '&X-Forwarded-For=%s'%_settings('ipaddress')
 
-    #web_pdb.set_trace()
     play_item.setPath(stream_url)
+
+    play_item.setProperty('inputstreamaddon', 'inputstream.adaptive')
+
+    if (stream_url.find('mpd') != -1):
+        play_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+        play_item.setMimeType('application/dash+xml')
+    else:
+        play_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        play_item.setMimeType('application/vnd.apple.mpegurl')
+
+    if license!="":
+        play_item.setProperty('inputstream.adaptive.license_type', 'com.widevine.alpha')
+        play_item.setProperty('inputstream.adaptive.license_key', license+ '|%s&Content-Type=application/octet-stream|R{SSM}|' % urlencode(headers))
+        play_item.setProperty("inputstream.adaptive.stream_headers", urlencode(headers))
+    play_item.setContentLookup(False)
+    
+
     # Pass the item to the Kodi player.
     xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
 
